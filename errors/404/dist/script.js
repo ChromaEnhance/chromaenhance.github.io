a8de9b810b0a35802676e32191afbc4825c73ce1
ChromaEnhance <chroma.enhance@outlook.com>
2025-11-22 19:59:20 +0100

    Refactor mouth animations to use scaling

--- errors/404/dist/script.js
+++ errors/404/dist/script.js
@@ -1,4 +1,4 @@
-// Colors and mouth shapes
+// Colors and mouth shapes (convertis en coordonnées pour animation CSS)
 var 
     furLightColor = "#FFF",
     furDarkColor = "#67b1e0",
@@ -9,16 +9,12 @@ var
     lettersFrontLight = "#67B1E0",
     lettersFrontDark = "#051d2c",
     lettersStrokeLight = "#265D85",
-    lettersStrokeDark = "#031219",
-    mouthShape1 = "M149 115.7c-4.6 3.7-6.6 9.8-5 15.6.1.5.3 1.1.5 1.6.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.4-1-.7-1.5-2.8-5.2-8.4-8.3-14.1-7.9-3.7.2-5.9 1.1-8.7 3.2z",
-    mouthShape2 = "M161.2 118.9c0 2.2-1.8 4-4 4s-4-1.8-4-4c0-1 .4-2 1.1-2.7.7-.8 1.8-1.3 2.9-1.3 2.2 0 4 1.7 4 4z",
-    mouthShape3 = "M150.2 118.3c-4.6 3.7-7.5 6.4-6.3 12.3.1.5.1.6.3 1.1.6 1.5 2.4 2.3 3.9 1.7 0 0 7.9-4.3 10.7-5.5s11.6-3.3 11.6-3.3c1.5-.6 2.3-2.4 1.7-3.9-.2-.5-.2-.6-.4-1.1-2.8-5.2-7.1-4.9-12.9-4.6-3.7.4-6.3 1.5-8.6 3.3z",
-    mouthShape4 = "M149.2 116.7c-4.6 3.7-6.7 8.8-5.2 14.6.1.3.1.5.2.8.6 1.5 2.4 2.3 3.9 1.7l11.2-4.4 11.2-4.4c1.5-.6 2.3-2.4 1.7-3.9-.1-.3-.2-.5-.4-.7-2.8-5.2-8.2-7.2-14-6.9-3.6.2-5.9 1.1-8.6 3.2z";
+    lettersStrokeDark = "#031219";
 
-// Mouth chatter timeline
+// Mouth chatter timeline - utilise scale au lieu de morph
 var chatterTL = gsap.timeline({paused: true, repeat: -1, yoyo: true});
 chatterTL
-    .to(['#mouthBG', '#mouthPath', '#mouthOutline'], {duration: 0.1, morphSVG: mouthShape4}, 0)
+    .to('#mouth', {duration: 0.1, scaleY: 0.8, transformOrigin: "center"}, 0)
     .to('#chin', {duration: 0.1, y: 1.5}, 0);
 
 // Yeti main timeline
@@ -39,14 +35,14 @@ yetiTL
     .call(goDark, null, 3.3)
     .call(goLight, null, 3.4)
 
-    // Pause chatter and reset mouth
+    // Pause chatter and change mouth
     .call(() => {
         chatterTL.pause();
-        gsap.to(['#mouthBG', '#mouthPath', '#mouthOutline'], {duration: 0.1, morphSVG: mouthShape1});
+        gsap.to('#mouth', {duration: 0.1, scaleY: 1, transformOrigin: "center"});
     }, null, 3.2)
 
-    // Mouth, tooth, arm, and eyes animation
-    .to(['#mouthBG', '#mouthPath', '#mouthOutline'], {duration: 0.25, morphSVG: mouthShape2}, 5)
+    // Mouth (scale instead of morph), tooth, arm, and eyes animation
+    .to('#mouth', {duration: 0.25, scaleY: 0.4, scaleX: 0.8, transformOrigin: "center"}, 5)
     .to('#tooth1', {duration: 0.1, y: -5}, 5)
     .to('#armR', {duration: 0.5, x: 10, y: 30, rotation: 10, transformOrigin: "bottom center", ease: "power1.out"}, 4)
     .to(['#eyeL', '#eyeR'], {duration: 0.25, scaleX: 1.4, scaleY: 1.4, transformOrigin: "center center"}, 5)
@@ -59,7 +55,7 @@ yetiTL
     .call(goDark, null, 8.6)
 
     // Reset mouth, tooth, arm, and eyes
-    .to(['#mouthBG', '#mouthPath', '#mouthOutline'], {duration: 0.25, morphSVG: mouthShape1}, 9)
+    .to('#mouth', {duration: 0.25, scaleY: 1, scaleX: 1, transformOrigin: "center"}, 9)
     .to('#tooth1', {duration: 0.1, y: 0}, 9)
     .to('#armR', {duration: 0.5, x: 0, y: 0, rotation: 0, transformOrigin: "bottom center", ease: "power1.out"}, 9)
     .to(['#eyeL', '#eyeR'], {duration: 0.25, scaleX: 1, scaleY: 1, transformOrigin: "center center"}, 9)
